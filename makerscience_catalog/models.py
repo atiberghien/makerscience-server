36f1d6696bc9cd2c2254d927211116d832e8279f
Alban <alban.tiberghien@gmail.com>
2014-11-28 11:57:20 +0000

    Add specific field for MakerscienceResource

--- makerscience_catalog/models.py
+++ makerscience_catalog/models.py
@@ -9,6 +9,18 @@ class MakerScienceProject(models.Model):
     
 
 class MakerScienceResource(models.Model):
+    
+    LEVEL_CHOICES = (
+        ("1", "Facile"),
+        ("2", "Moyen"),
+        ("3", "Difficile")
+    )
+    
     parent = models.ForeignKey(Project)
     tags = TaggableManager()
     modified = models.DateTimeField()
+    
+    level = models.CharField(max_length=1, choices=LEVEL_CHOICES)
+    duration = models.CharField(max_length=30)
+    cost = models.PositiveIntegerField(default=0)
+    
