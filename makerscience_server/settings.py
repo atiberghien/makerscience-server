1d7a46fe1363d65a9cbc5e46cb99146d1e4e912f
Freddy Limpens <freddy.limpens@gmail.com>
2015-02-18 11:07:49 +0100

    added ucomment app

--- makerscience_server/settings.py
+++ makerscience_server/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+ADMINS = (
+    ('Freddy', 'freddy.limpens@gmail.com'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -75,6 +78,7 @@ INSTALLED_APPS = (
     'scout',
     'multiuploader',
     'graffiti',
+    'ucomment',
     
     'projects',
     'projectsheet',
