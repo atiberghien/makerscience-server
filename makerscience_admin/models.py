b4c9b76d132668695b77d37d7db3071e629fcba7
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-06-23 14:34:42 +0200

    Allow to clear useless instances

--- makerscience_admin/models.py
+++ makerscience_admin/models.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.db.models.signals import post_delete
+
 from solo.models import SingletonModel
+
+from accounts.models import ObjectProfileLink
+
+from makerscience_forum.models import MakerSciencePost
 
 class MakerScienceStaticContent (SingletonModel):
     about = models.TextField(null=True, blank=True)
@@ -13,3 +19,12 @@ class MakerScienceStaticContent (SingletonModel):
 class PageViews(models.Model):
     client = models.CharField(max_length=255)
     resource_uri = models.CharField(max_length=255)
+
+
+def clear_makerscience(sender, instance, **kwargs):
+    if sender == MakerSciencePost:
+        ObjectProfileLink.objects.filter(content_type__model='post',
+                                      object_id=instance.parent.id).delete()
+        instance.parent.delete()
+
+post_delete.connect(clear_makerscience, sender=MakerSciencePost)
