437e0b03e88b381136d9889dae9786ba6d547c5d
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-08-03 11:27:53 +0200

    connect to elasticsearch

--- makerscience_server/site_settings.py
+++ makerscience_server/site_settings.py
@@ -29,11 +29,12 @@ DATABASES = {
 # Haystack (for bucket module)
 HAYSTACK_CONNECTIONS = {
     'default': {
-        'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
-        'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
-	#'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
-	#'URL': 'http://127.0.0.1:9200/',
-        'INDEX_NAME': 'bucket',
+        # 'ENGINE': 'haystack.backends.whoosh_backend.WhooshEngine',
+        # 'PATH': os.path.join(os.path.dirname(__file__), 'whoosh_index'),
+        'ENGINE': 'haystack.backends.elasticsearch_backend.ElasticsearchSearchEngine',
+        'URL': 'http://127.0.0.1:9200/',
+        'INDEX_NAME': 'haystack',
+        # 'INDEX_NAME': 'bucket',
     },
 }
 
