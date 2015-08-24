13c630b1015bce7a02e7b985af97708a97fbef14
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-08-24 11:27:02 +0200

    Delete place and adress on profile deletion

--- makerscience_admin/models.py
+++ makerscience_admin/models.py
@@ -37,6 +37,8 @@ def clear_makerscience(sender, instance, **kwargs):
                                       object_id=instance.parent.id).delete()
         instance.parent.delete()
     if sender == MakerScienceProfile:
+        instance.location.address.delete()
+        instance.location.delete()
         instance.parent.user.delete()
 
 post_delete.connect(clear_makerscience, sender=MakerSciencePost)
