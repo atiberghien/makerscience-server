b17fbe421cf47f2c3e3882965ddefd96784d9720
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-09-16 00:04:44 +0200

    Add guardian modeladmin

--- makerscience_profile/admin.py
+++ makerscience_profile/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import MakerScienceProfile
 from django.contrib.admin.options import ModelAdmin
 from accounts.models import Profile
+from guardian.admin import GuardedModelAdmin
 
-
-admin.site.register(MakerScienceProfile, ModelAdmin)
+admin.site.register(MakerScienceProfile, GuardedModelAdmin)
