b5943c700db6a4f866e370ec3bb12b130ba4d045
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-09-24 19:03:55 +0200

    Avoid anonymous notification fetching

--- makerscience_notification/api.py
+++ makerscience_notification/api.py
@@ -40,4 +40,7 @@ class NotificationResource(ModelResource):
         return render_to_string('notifications/notification.html', {'notif': bundle.obj})
 
     def apply_filters(self, request, applicable_filters):
+        if request.user.is_anonymous():
+            return ModelResource.apply_filters(self, request, applicable_filters)
+
         return self.get_object_list(request).filter(**applicable_filters).exclude(actor_object_id=MakerScienceProfile.objects.get(parent__user=request.user).id)
