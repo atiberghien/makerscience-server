29f37783b597412818e259abb823e76ce9116781
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-10-12 19:23:42 +0200

    Fix missing context var

--- makerscience_notification/models.py
+++ makerscience_notification/models.py
@@ -175,8 +175,8 @@ def send_notifications_by_mail(frequency):
             subject = "Notifications Makerscience"
             from_email = 'no-reply@makerscience.fr'
             to = profile.parent.user.email
-            text_content = render_to_string('notifications/notif_multiple.txt', {'notifs' : notifs})
-            html_content = render_to_string('notifications/notif_multiple.html', {'notifs' : notifs})
+            text_content = render_to_string('notifications/notif_multiple.txt', {'frequency' : frequency, 'notifs' : notifs})
+            html_content = render_to_string('notifications/notif_multiple.html', {'frequency' : frequency, 'notifs' : notifs})
             msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
             msg.attach_alternative(html_content, "text/html")
             msg.send()
