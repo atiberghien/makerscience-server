a7d5290f4ed40a08e06641b98417850d146e4573
Alban Tiberghien <alban.tiberghien@gmail.com>
2015-12-14 21:46:11 +0100

    Fix notification recipients

--- makerscience_notification/models.py
+++ makerscience_notification/models.py
@@ -113,7 +113,10 @@ def create_notification(sender, instance, created, **kwargs):
                         target=activity.content_object,
                         verb=u'help_invited')
         elif activity.level == 7: #someone added a news to a project
-            profile_ids = ObjectProfileLink.objects.filter(level__in=[0, 1, 2]).distinct('profile').values_list('profile', flat=True)
+            profile_ids = ObjectProfileLink.objects.filter(level__in=[0, 1, 2],
+                                                           isValidated=True,
+                                                           content_type=ContentType.objects.get_for_model(activity.content_object),
+                                                           object_id=activity.content_object.id).distinct('profile').values_list('profile', flat=True)
 
             for profile in MakerScienceProfile.objects.filter(parent__id__in=filter(is_not_actor, profile_ids)):
                 notify.send(actor,
@@ -131,7 +134,10 @@ def create_notification(sender, instance, created, **kwargs):
                         target=activity.content_object,
                         verb=u'similar_resource_invited')
         elif activity.level in [2, 12, 33]: #liked content where the recipient is involved (creator, member)
-            profile_ids = ObjectProfileLink.objects.filter(level__in=[0, 1, 10, 11, 30, 31]).distinct('profile').values_list('profile', flat=True)
+            profile_ids = ObjectProfileLink.objects.filter(level__in=[0, 1, 10, 11, 30, 31],
+                                                           isValidated=True,
+                                                           content_type=ContentType.objects.get_for_model(activity.content_object),
+                                                           object_id=activity.content_object.id).distinct('profile').values_list('profile', flat=True)
 
             for profile in MakerScienceProfile.objects.filter(parent__id__in=filter(is_not_actor, profile_ids)):
                 notify.send(actor,
@@ -142,7 +148,10 @@ def create_notification(sender, instance, created, **kwargs):
 
             target_level = 0 if activity.level  == 3 else 10
 
-            profile_ids = ObjectProfileLink.objects.filter(level=target_level).distinct('profile').values_list('profile', flat=True)
+            profile_ids = ObjectProfileLink.objects.filter(level=target_level,
+                                                           isValidated=True,
+                                                           content_type=ContentType.objects.get_for_model(activity.content_object),
+                                                           object_id=activity.content_object.id).distinct('profile').values_list('profile', flat=True)
 
             for profile in MakerScienceProfile.objects.filter(parent__id__in=filter(is_not_actor, profile_ids)):
                 notify.send(actor,
@@ -150,7 +159,10 @@ def create_notification(sender, instance, created, **kwargs):
                             action_object=activity.content_object,
                             verb=u'commented')
         elif activity.level in [4, 14]: #scored content where the recipient is involved (creator, member)
-            profile_ids = ObjectProfileLink.objects.filter(level__in=[0, 1, 10, 11]).distinct('profile').values_list('profile', flat=True)
+            profile_ids = ObjectProfileLink.objects.filter(level__in=[0, 1, 10, 11],
+                                                           isValidated=True,
+                                                           content_type=ContentType.objects.get_for_model(activity.content_object),
+                                                           object_id=activity.content_object.id).distinct('profile').values_list('profile', flat=True)
 
             for profile in MakerScienceProfile.objects.filter(parent__id__in=filter(is_not_actor, profile_ids)):
                 notify.send(actor,
